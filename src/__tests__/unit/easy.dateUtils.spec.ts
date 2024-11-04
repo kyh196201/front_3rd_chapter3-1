04cd0bf679ce619d5b9c5cb3c7805fed591d7068
seungwoo Kim <kyh196201@gmail.com>
2024-11-04 23:18:22 +0900

    test: formatMonth 테스트 코드 구현

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -237,7 +237,11 @@ describe('formatWeek', () => {
 });
 
 describe('formatMonth', () => {
-  it("2024년 7월 10일을 '2024년 7월'로 반환한다", () => {});
+  it("2024년 7월 10일을 '2024년 7월'로 반환한다", () => {
+    const date = new Date(2024, 6, 10);
+
+    expect(formatMonth(date)).toBe('2024년 7월');
+  });
 });
 
 describe('isDateInRange', () => {
