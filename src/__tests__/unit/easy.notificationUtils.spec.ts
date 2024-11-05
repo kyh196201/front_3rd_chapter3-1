5422efb9b849a0efbab327907b657624388bb997
seungwoo Kim <kyh196201@gmail.com>
2024-11-06 00:21:49 +0900

    test: notificationUtils 테스트 코드 구현

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -1,16 +1,183 @@
 import { Event } from '../../types';
+import { parseDateTime } from '../../utils/eventOverlap';
 import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
 
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
+  beforeEach(() => {
+    vi.setSystemTime(parseDateTime('2024-10-15', '08:50:00'));
+  });
 
-  it('이미 알림이 간 이벤트는 제외한다', () => {});
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2024-10-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+      {
+        id: '2',
+        title: '기존 회의',
+        date: '2024-10-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 5,
+      },
+    ];
 
-  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {});
+    const filtered = getUpcomingEvents(events, new Date(), []);
 
-  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {});
+    expect(filtered).toEqual([
+      expect.objectContaining({
+        id: '1',
+      }),
+    ]);
+  });
+
+  it('이미 알림이 간 이벤트는 제외한다', () => {
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2024-10-15',
+        startTime: '08:55',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+      {
+        id: '2',
+        title: '기존 회의',
+        date: '2024-10-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ];
+
+    const filtered = getUpcomingEvents(events, new Date(), ['1']);
+
+    expect(filtered).toHaveLength(1);
+    expect(filtered).toEqual([
+      expect.objectContaining({
+        id: '2',
+      }),
+    ]);
+  });
+
+  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2024-10-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+      {
+        id: '2',
+        title: '기존 회의',
+        date: '2024-10-16',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ];
+
+    const filtered = getUpcomingEvents(events, new Date(), []);
+
+    expect(filtered).toEqual([
+      expect.objectContaining({
+        id: '1',
+      }),
+    ]);
+  });
+
+  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2024-10-14',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+      {
+        id: '2',
+        title: '기존 회의',
+        date: '2024-10-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ];
+
+    const filtered = getUpcomingEvents(events, new Date(), []);
+
+    expect(filtered).toEqual([
+      expect.objectContaining({
+        id: '2',
+      }),
+    ]);
+  });
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  it('올바른 알림 메시지를 생성해야 한다', () => {
+    const event: Event = {
+      id: '1',
+      title: '기존 회의',
+      date: '2024-10-15',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    };
+
+    const message = createNotificationMessage(event);
+
+    // expect(message).toBe(`${event.notificationTime}분 후 ${event.title} 일정이 시작됩니다.`);
+
+    // MEMO: 질문: 이렇게 정규식을 이용해서 반환 값을 검증해도 괜찮을까요?
+    expect(message).toMatch(/10분 후/);
+    expect(message).toMatch(/기존 회의/);
+  });
 });
