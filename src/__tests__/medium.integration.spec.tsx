4ef8c1fa75203cfb7ad58d9054c6f24f4b021752
kyh196201 <kyh196201@gmail.com>
2024-11-07 22:44:58 +0900

    test: 통합 테스트 일정 수정 테스트 코드 구현

--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -12,6 +12,7 @@ import {
 import App from '../App';
 import { server } from '../setupTests';
 import { Event } from '../types';
+import { createDate } from './utils';
 
 // ! HINT. 이 유틸을 사용해 리액트 컴포넌트를 렌더링해보세요.
 const setup = (element: ReactElement) => {
@@ -42,7 +43,7 @@ const saveSchedule = async (
 
 // ! HINT. "검색 결과가 없습니다"는 초기에 노출되는데요. 그럼 검증하고자 하는 액션이 실행되기 전에 검증해버리지 않을까요? 이 테스트를 신뢰성있게 만드려면 어떻게 할까요?
 describe('일정 CRUD 및 기본 기능', () => {
-  it.only('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
+  it('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
     // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
     setupMockHandlerCreation();
 
@@ -68,7 +69,48 @@ describe('일정 CRUD 및 기본 기능', () => {
     });
   });
 
-  it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {});
+  it.only('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {
+    vi.setSystemTime(createDate('2024-10-15'));
+
+    setupMockHandlerUpdating();
+
+    const { user } = setup(<App />);
+
+    const editButtons = await screen.findAllByLabelText('Edit event');
+
+    // 1. 수정 버튼 클릭
+    await user.click(editButtons[0]);
+
+    // 2. 폼에 데이터 잘 반영되었는지 확인
+    const titleInput = screen.getByLabelText('제목');
+    expect(titleInput).toHaveValue('기존 회의');
+
+    // 3. 제목 수정
+    const newTitle = '새로운 회의';
+    await user.clear(titleInput);
+
+    // MEMO: user.clear()를 하지 않을 경우
+    await user.type(titleInput, newTitle);
+
+    const dateInput = screen.getByLabelText('날짜');
+    const newDate = '2024-10-17';
+    await user.clear(dateInput);
+    await user.type(dateInput, newDate);
+
+    // 4. 업데이트 실행
+    await user.click(screen.getByTestId('event-submit-button'));
+
+    // 5. 데이터 반영 확인
+    expect(titleInput).toHaveValue('');
+
+    await waitFor(() => {
+      // MEMO: calendar 영역에도 추가되기 때문에 2개의 요소가 검색되고, 테스트가 실패함
+      // event-list 안에서 요소를 검색하기 위해서 within 사용
+      const eventList = screen.getByTestId('event-list');
+      expect(within(eventList).getByText(newTitle)).toBeInTheDocument();
+      expect(within(eventList).getByText(newDate)).toBeInTheDocument();
+    });
+  });
 
   it('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {});
 });
